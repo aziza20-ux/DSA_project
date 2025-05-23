7f85804364803b9d55eff2351cab60d0701af572
aziza20-ux <s.afadhali@alustudent.com>
2025-05-23 14:18:55 +0200

    the results

--- dsa/sparse_matrix/code/src/sparse_matrix.py
+++ dsa/sparse_matrix/code/src/sparse_matrix.py
@@ -1,4 +1,49 @@
+
+"""
+this is class representation to save sparse matrix which has non-zeros
+as means of saving memory.
+
+1.`load_from_file`(the class constructor):
+    this function as class constructor to build an instance
+     here `matrix = cls(n_rows, n_cols)` from input files
+
+2.`getElement():
+
+    the method to retrieve a value from certain position
+    to carry operation on it
+
+3.`setElement()`:
+    the method to save non-zero elements in dictionary
+    and skip zeros as meaans of saving memory
+
+
+4.`add`():
+    the function which loops through keys of both matrices
+    and add the value of position they have in common ans retain others
+5.`substract`:
+    the function which loops through keys of both matrices
+    and substruct the value of position they have in common ans retain others
+6.`multiply()`:
+    this `multiply(self, matrix_B)` first checks if the columns of the first
+    matrix is equal to the number of rows in the second matrix
+    and loops through both matrices to
+    maltiply each element on the first matrix column with the second's columns element
+7.`write_to_file()`:
+    the method to write the result of substraction, addition or multiplication
+    of two matrices to a disered file also entered by a user
+8.`main()`:
+    the main function to get inputs from user
+and print the results according to user's preferences
+
+
+"""
+
+
 class SparseMatrix():
+
+    # the constructor of a class to
+    # build matrix instance
+
     def __init__(self, n_rows, n_cols):
         self.n_rows = n_rows
         self.n_cols = n_cols
@@ -17,15 +62,24 @@ class SparseMatrix():
     @classmethod
     def load_from_file(cls, file_path):
 
+        # this try block parses through matrix lines and remove whitespace, and
+        # ignore empty lines
         try:
             with open(file_path, 'r') as f:
                 lines = [line.strip() for line in f if line.strip() != '']
         except FileNotFoundError:
             raise FileNotFoundError("file {file_path} doesn't exist")
+
+        # this trims the number of rows and columns
+
         n_rows = int(lines[0].split('=')[1])
         n_cols = int(lines[1].split('=')[1])
 
+        # creates an instance of `SparseMatrix` class
+
         matrix = cls(n_rows, n_cols)
+        # loops through the lines to check if they folllow the format
+        # and make a list of rows and columns matrix
         for line in lines[2:]:
             if not (line.startswith('(') and line.endswith(')')):
                 raise ValueError("this sparsematrix doen't follow the format")
@@ -33,10 +87,10 @@ class SparseMatrix():
                 line = line.strip()[1:-1]
                 row, col, value = line.split(',')
                 row, col, value = int(row), int(col), int(value)
-
+                # adds the values to matrix
                 matrix.setElements(row, col, value)
             except Exception:
-                raise ValueError ("wrong format input")
+                raise ValueError("wrong format input")
         return matrix
 
     def add(self, matrix_B):
@@ -64,17 +118,24 @@ class SparseMatrix():
         return result
 
     def multiply(self, matrix_B):
-        if self.n_cols != matrix_B.n_rows:
-            raise ValueError("the columns of A is unequal to row")
-        result = SparseMatrix(self.n_rows, matrix_B.n_cols)
 
-        for (i, k), val in self.data.items():
-            for j in range(matrix_B.n_cols):
-                val2 = matrix_B.getElements(k, j)
-                if val2 != 0:
-                    curr = result.getElements(i, j)
-                    result.setElements(i, j, curr + val * val2)
-        return result
+          if self.n_cols != matrix_B.n_rows:
+
+
+              raise ValueError("the columns of A is unequal to row")
+          result = SparseMatrix(self.n_rows, matrix_B.n_cols)
+
+          for (i, k), val in self.data.items():
+
+             for j in range(matrix_B.n_cols):
+                 val2 = matrix_B.getElements(k, j)
+                 if val2 != 0:
+                     curr = result.getElements(i, j)
+                     result.setElements(i, j, curr + val * val2)
+          return result
+
+    #the method to write the result of substraction, addition or multiplication
+    #of two matrices to a disered file also entered by a user"""
 
     def write_to_file(self, file_path):
 
@@ -87,15 +148,16 @@ class SparseMatrix():
         except Exception:
             raise ValueError("no file found please provide one")
 
-
+#the main function to get inputs from user
+#and print the results according to user's preferences
 def main():
 
     print("play with these operations\n")
     print("choose among these operations: add/substract/multiply")
-    choice = input("enter your choice\n").strip().lower()
+    choice = input("enter your choice:").strip().lower()
 
-    fil1 = input("enter the first matrix file path\n").strip()
-    fil2 = input("enter the second matrix file path\n").strip()
+    fil1 = input("enter the first matrix file path:").strip()
+    fil2 = input("enter the second matrix file path:").strip()
 
     A = SparseMatrix.load_from_file(fil1)
     B = SparseMatrix.load_from_file(fil2)
@@ -115,7 +177,7 @@ def main():
                 "enter a file path where to see your result:").strip()
         result.write_to_file(result_file)
 
-        print("successfull!!", result_file)
+        print("successfull!! check your results here:", result_file)
 
     except Exception as e:
         print("ERROR:", e)
